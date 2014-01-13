b2986fe0c88ecc5053de2578b1d6de8c32fac7dd
Camilo Sperberg <me@unreal4u.com>
2014-01-14 00:16:13 +0100

    Many more tests
    - Small bug fix on throwing exceptions
    - .gitattributes to exclude some dirs when archiving
    - Updated documentation

--- src/unreal4u/pid.php
+++ src/unreal4u/pid.php
@@ -11,7 +11,7 @@ include(dirname(__FILE__).'/auxiliar_classes.php');
  * @subpackage PIDVerifier
  * @author Camilo Sperberg - http://unreal4u.com/
  * @author http://www.electrictoolbox.com/check-php-script-already-running/
- * @version 1.4.1
+ * @version 1.4.2
  * @license BSD License. Feel free to modify
  * @throws pidException
  */
@@ -21,7 +21,7 @@ class pid {
      * The version of this class
      * @var string
      */
-    private $_version = '1.4.1';
+    private $_version = '1.4.2';
 
     /**
      * The filename of the PID
@@ -199,7 +199,7 @@ class pid {
      */
     protected function throwException($msg='', $line=0) {
         if (empty($this->supressErrors)) {
-            throw new unreal4u\pidException($msg, $line, __FILE__);
+            throw new pidException($msg, $line, __FILE__);
         }
 
         return false;
