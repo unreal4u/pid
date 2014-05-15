51b5d609c369d6939dbe9dc703e2ff03d3896cde
Camilo Sperberg <me@unreal4u.com>
2014-05-15 15:54:55 +0200

    Documentation fixes
    Better documentation and typo in example fix

--- examples/complex-example.php
+++ examples/complex-example.php
@@ -7,10 +7,10 @@ class complexExample {
     private $_pid = null;
 
     public function __construct($timeout=30) {
-        $this->pid = new unreal4u\pid(false);
+        $this->_pid = new unreal4u\pid(false);
 
         try {
-            $this->pid->checkPid('', 'myVeryOwnName', $timeout);
+            $this->_pid->checkPid('', 'myVeryOwnName', $timeout);
         } catch (unreal4u\alreadyRunningException $e) {
             // Ok, you should never call die or exit within your script, but this is just an example file
             die($e->getMessage().PHP_EOL);
@@ -19,23 +19,24 @@ class complexExample {
         } catch (unreal4u\pidException $e) {
             die('Error detected: '.$e->getMessage().PHP_EOL);
         } catch (\Exception $e) {
+            // A normal Exception should not happen too often, but MAY occur anyway in the future
             die('Any other exception: '.$e->getMessage().PHP_EOL);
         }
 
-        if (!$this->pid->alreadyRunning) {
-        	$this->runForLong($timeout);
+        if (!$this->_pid->alreadyRunning) {
+            $this->runForLong($timeout);
         } else {
-        	throw new Exception(sprintf('Process already running with pid %s', $this->pid->pid).PHP_EOL);
+            throw new Exception(sprintf('Process already running with pid %s', $this->_pid->pid).PHP_EOL);
         }
     }
 
     public function runForLong($maxSeconds) {
-        longRunningFunction($maxSeconds, $this->pid->pid);
+        longRunningFunction($maxSeconds, $this->_pid->pid);
     }
 }
 
 try {
-	$complexExample = new complexExample(30);
+    $complexExample = new complexExample(30);
 } catch (\Exception $e) {
-	die($e->getMessage());
+    die($e->getMessage());
 }
